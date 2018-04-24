e2ca6a900e7aca035f547367cdd213944427a0b4
DevExpressExampleBot <fakeEmail>
2018-04-24 20:07:44 +0300

    Source auto update [skip ci]

--- VB/WinWebSolution.Module/Child.vb
+++ VB/WinWebSolution.Module/Child.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports DevExpress.Xpo
 Imports DevExpress.ExpressApp
 Imports DevExpress.Persistent.Base
@@ -7,30 +6,31 @@ Imports DevExpress.Persistent.BaseImpl
 Imports DevExpress.Persistent.Validation
 
 Namespace WinWebSolution.Module
-	<DeferredDeletion(False)> _
-	Public Class Child
-		Inherits BaseObject
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
-		Private _name As String
-		Public Property Name() As String
-			Get
-				Return _name
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue("Name", _name, value)
-			End Set
-		End Property
-		Private _master As Master
-		<Association("Master-Children")> _
-		Public Property Master() As Master
-			Get
-				Return _master
-			End Get
-			Set(ByVal value As Master)
-				SetPropertyValue("Master", _master, value)
-			End Set
-		End Property
-	End Class
+    <DeferredDeletion(False)> _
+    Public Class Child
+        Inherits BaseObject
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+        Private _name As String
+        Public Property Name() As String
+            Get
+                Return _name
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Name", _name, value)
+            End Set
+        End Property
+        Private _master As Master
+        <Association("Master-Children")> _
+        Public Property Master() As Master
+            Get
+                Return _master
+            End Get
+            Set(ByVal value As Master)
+                SetPropertyValue("Master", _master, value)
+            End Set
+        End Property
+    End Class
 End Namespace
